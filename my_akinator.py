d1000f86229f62e38b30daaa54380100b2c756e3
Dimkqo <146217024+Dimkqo@users.noreply.github.com>
2023-11-11 14:41:16 +0700

    Обновление базы персонажей
    - Добавил персонажей и вопросы
    - Исправил некоторые неточности

--- my_akinator.py
+++ my_akinator.py
@@ -3,19 +3,22 @@ import random
 import main
 import voice
 
-def init_bases():
-        persons = pandas.read_csv('desktop/assistant-main/big_scary_db.csv', sep=';')
-        #persons = pandas.read_csv('big_scary_db.csv', sep=';')
+#чтение таблиц из файлов
+def load_bases():
+        #persons = pandas.read_csv('desktop/assistant-main/big_scary_db.csv', sep=';')
+        persons = pandas.read_csv('big_scary_db.csv', sep=';')
         questions = {}
-        file = open('desktop/assistant-main/db_questions.txt', 'r')
-        #file = open('db_questions.txt', 'r')
+        #file = open('desktop/assistant-main/db_questions.txt', 'r')
+        file = open('db_questions.txt', 'r')
         for i in file.readlines():
             questions[i.split('|')[0]] = i.split('|')[1].replace('\n','')
         file.close()
         return persons, questions
 
+#предлог сыграть заново
 def try_again():
     message('я не знаю подобного персонажа, загадаете ещё')
+    confirm = None
     while confirm == None:
         confirm = main.get_string()
     if confirm == 'да':
@@ -23,8 +26,10 @@ def try_again():
     else:
         return False
     
+#вывести сообщение в консоль и произнести его
 def message(msg):
     print('джарвис:', msg)
+    msg.replace('м/с', 'мультсериала').replace('м/ф', 'мультфильма').replace('к/ф', 'кинофраншизы').replace('т/с', 'телесериала')
     voice.speaker(msg)
 
 def get_answer():
@@ -35,11 +40,12 @@ def get_answer():
 
 def start():
     stop_words = ['стоп', 'хватит', 'прекрати', 'давай закончим']
-    persons ,questions = init_bases()
+    persons ,questions = load_bases()
     q_num = 0
 
     while True:
         column = ''
+        #сначала идут типовые вопросы: мужчина или женшина, живой - мертвый, выымышленный или реальный
         q_num += 1
         if q_num == 1:
             if random.randint(0, 1) == 0:
@@ -47,35 +53,39 @@ def start():
             else:
                 column = 'Female'
         elif (q_num == 2):
-            if 'Imagine' in questions:
+            if random.randint(0, 1) == 0:
                 column = 'Imagine'
-            elif 'Real' in questions:
+            else:
                 column = 'Real'
-            else:
-                column = random.choice(list(questions.keys()))
         elif q_num == 3:
-            if 'Live' in questions:
+            if random.randint(0, 1) == 0:
                 column = 'Live'
-            elif 'Dead' in questions:
-                column = 'Dead'
             else:
-                column = random.choice(list(questions.keys()))
+                column = 'Dead'
         else:
             column = random.choice(list(questions.keys()))
-        message(questions[column])
+        #спрашиваем и убираем вопрос, чтобы не повторялся
+        if len(questions) > 0:
+            message(questions[column])
+        else:
+            message("мои вопросы не загрузились")
+            return
         del questions[column]
 
+        #получаем ответ
         data = get_answer()
-        #print('пользователь:', data)
 
+        #если стоп-слово, выходим
         if data in stop_words:
             break
 
+        #фильтруем персонажей
         if data == 'да':
             persons = persons[persons[column] == True]
         elif data == 'нет':
             persons = persons[persons[column] == False]
 
+        #убираем столбы с одним значением.
         cols = list(questions.keys())
         for i in cols:
             if (len(persons[i].unique()) == 1):
@@ -83,23 +93,25 @@ def start():
 
         print('персонажи', persons.shape[0]) #для дебага
         
-        if persons.shape[0] == 0:
-            if not try_again():
-                break
-
-        elif persons.shape[0] < 5:
+        #если осталось немного персонажей, случайно предлагаем из оставшегося
+        if (persons.shape[0] < 5) & (len(questions) < 4) | (persons.shape[0] == 1):
             while persons.shape[0] > 0:
                 pers = random.choice(list(persons.Name))
                 message('вы загадали '+pers)
+                #если персонаж не тот, убираем его. если угадали, предлагаем сыграть заново
                 if (get_answer() != 'нет'):
                     message('сыграем еще')
                     if get_answer() != 'да':
                         return
+                    break
                 persons = persons[persons['Name'] != pers]
             else:
+                #если персонажей не осталось, говорим, что такого не знаем, давай по новой
                 if not try_again():
                     return
-            persons ,questions = init_bases()
+            #грузим данные
+            persons ,questions = load_bases()
+            q_num = 0
         
 if __name__ == '__main__':
     start()
